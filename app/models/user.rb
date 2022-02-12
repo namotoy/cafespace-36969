d5c59400ab5bfd99711c209de471030b2bdc4a5e
namotoy <namotopet0615@gmail.com>
2022-02-12 19:02:47 +0900

    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  # validates :prefecture_id, presence: true
+  validates :city, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :prefecture_id, 
+            numericality: { other_than: 0, message: "can't be blank" }
 end

73eac4d17132c03e5c128f43c3cd76713d5d52a9
namotoy <namotopet0615@gmail.com>
2022-02-22 21:44:08 +0900

    アクションとルーティング設定

--- app/controllers/cafetypes_controller.rb
+++ app/controllers/cafetypes_controller.rb
@@ -1,4 +1,18 @@
 class CafetypesController < ApplicationController
   def index
+  
   end
+  def new
+    @cafetype = Cafetype.new
+  end
+
+  def create
+    Cafetype.create(cafetype_params)
+  end
+
+  private
+  def cafetype_params
+    params.require(:cafetype).permit(:shop_name, :image, :catch_copy, :prefecture_id, :city, :block_number, :wifi, :power_supply, :capacity, :toilet_place)
+  end
+
 end

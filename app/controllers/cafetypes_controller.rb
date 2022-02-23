a13c8466be9294632cc89e77492f736311849452
namotoy <namotopet0615@gmail.com>
2022-02-23 12:37:10 +0900

    ユーザーIDをマージ

--- app/controllers/cafetypes_controller.rb
+++ app/controllers/cafetypes_controller.rb
@@ -12,7 +12,7 @@ class CafetypesController < ApplicationController
 
   private
   def cafetype_params
-    params.require(:cafetype).permit(:shop_name, :image, :catch_copy, :prefecture_id, :city, :block_number, :wifi, :power_supply, :capacity, :toilet_place)
+    params.require(:cafetype).permit(:shop_name, :image, :catch_copy, :prefecture_id, :city, :block_number, :wifi, :power_supply, :capacity, :toilet_place).merge(user_id: current_user.id)
   end
 
 end

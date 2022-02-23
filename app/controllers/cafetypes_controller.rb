7062981a8f9b898bb7e5a8e648b034d117216137
namotoy <namotopet0615@gmail.com>
2022-02-23 12:57:37 +0900

    投稿データの保存

--- app/controllers/cafetypes_controller.rb
+++ app/controllers/cafetypes_controller.rb
@@ -7,12 +7,17 @@ class CafetypesController < ApplicationController
   end
 
   def create
-    Cafetype.create(cafetype_params)
+    @cafetype = Cafetype.create(cafetype_params)
+    if @cafetype.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def cafetype_params
-    params.require(:cafetype).permit(:shop_name, :image, :catch_copy, :prefecture_id, :city, :block_number, :wifi, :power_supply, :capacity, :toilet_place).merge(user_id: current_user.id)
+    params.require(:cafetype).permit(:shop_name, :image, :catch_copy, :prefecture_id, :city, :block_number, :wifi, :power_supply, :capacity, :toilet_place, :cafe_price).merge(user_id: current_user.id)
   end
 
 end

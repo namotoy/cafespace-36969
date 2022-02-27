34eed27488e06cb68beeb58f03d8a7b9ed69b5cd
namotoy <namotopet0615@gmail.com>
2022-02-27 18:28:27 +0900

    URLを直接入力によるアクセス防止

--- app/controllers/cafetypes_controller.rb
+++ app/controllers/cafetypes_controller.rb
@@ -1,8 +1,10 @@
 class CafetypesController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :set_cafetype, except: [:index, :new, :create]
+  before_action :authenticate_user!, expect: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   
   def index
-    @cafetypes = Cafetype.all.order('created_at DESC')
+    @cafetypes = Cafetype.includes(:user).order('created_at DESC')
   end
   
   def new
@@ -19,15 +21,15 @@ class CafetypesController < ApplicationController
   end
 
   def show
-    @cafetype = Cafetype.find(params[:id])
+   
   end
 
   def edit
-    @cafetype = Cafetype.find(params[:id])
+    
   end
   
   def update
-    @cafetype = Cafetype.find(params[:id])
+    
     if @cafetype.update(cafetype_params)
       redirect_to cafetype_path
     else
@@ -40,6 +42,16 @@ class CafetypesController < ApplicationController
   private
   def cafetype_params
     params.require(:cafetype).permit(:shop_name, :image, :catch_copy, :prefecture_id, :city, :block_number, :wifi_id, :power_supply_id, :capacity_id, :toilet_place_id, :cafe_price).merge(user_id: current_user.id)
+  end
+
+  def set_cafetype
+    @cafetype = Cafetype.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @cafetype.user
+      redirect_to root_path
+    end
   end
 
 end

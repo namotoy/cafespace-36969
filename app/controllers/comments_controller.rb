d264ffe2827f967086222bff133df2bc8618d83a
namotoy <namotopet0615@gmail.com>
2022-03-12 12:13:59 +0900

    コントローラーの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to cafetype_path(params[:cafetype_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.user.id, cafetype_id: params[:cafetype_id])
+  end
 end

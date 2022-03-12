f8f55c7150ffb6f478111186a23520aaef858087
namotoy <namotopet0615@gmail.com>
2022-03-12 11:55:55 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'cafetypes/index'
   root to: "cafetypes#index"
   resources :cafetypes do
+    resources :comments, only: :create
     collection do
       get 'search'
     end
